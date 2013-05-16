a49cdca2399fa8648483125c6876a279ffa82981
anandan <andy.compeer@gmail.com>
2013-05-15 20:28:40 -0400

    Parse a given json path from a given json file

--- src/main/java/com/grooveshark/util/FileUtils.java
+++ src/main/java/com/grooveshark/util/FileUtils.java
@@ -1,12 +1,19 @@
 package com.grooveshark.util;
 
 import java.util.List;
+import java.util.Set;
 import java.util.LinkedList;
+import java.util.TreeMap;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapred.JobConf;
@@ -65,5 +72,29 @@ public class FileUtils
         } else {
             return false;
         }
+    }
+
+    public static String parseJson(String filename, String[] jsonPath) throws Exception {
+        Gson gson = new Gson();
+        BufferedReader br = new BufferedReader( new FileReader(filename) );
+        TreeMap file = gson.fromJson(br, TreeMap.class);
+        JsonObject jo = gson.toJsonTree(file).getAsJsonObject();
+        JsonElement jp = null;
+        for (int i = 0; i < jsonPath.length; i++) {
+            String p = jsonPath[i];
+            if (jo.has(p)) {
+                if (i == jsonPath.length - 1) {
+                    jp = jo.get(p);
+                    break;
+                }
+                jo = jo.getAsJsonObject(p);
+            } else {
+                throw new Exception("Json file does not contain the string '" + p + "' specified in the path");
+            }
+        }
+        if (jp.isJsonObject()) {
+            return jp.toString();
+        }
+        return jp.getAsString();
     }
 }
