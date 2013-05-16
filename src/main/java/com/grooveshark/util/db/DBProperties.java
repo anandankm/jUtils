3175368f73b9271736ed52e014c2513c00ce812b
anandan <andy.compeer@gmail.com>
2013-05-16 12:11:05 -0400

    Add default db properties for package build

---
+++ src/main/java/com/grooveshark/util/db/DBProperties.java
@@ -0,0 +1,20 @@
+package com.grooveshark.util.db;
+
+public class DBProperties
+{
+    public static String DEFAULT_MYSQL_DRIVER = "";
+    public static String DEFAULT_MYSQL_URL = "";
+    public static String DEFAULT_MYSQL_USER = "";
+    public static String DEFAULT_MYSQL_PASS = "";
+    public static String DEFAULT_MONGO_HOST = "";
+    public static int DEFAULT_MONGO_PORT = 2718;
+    public static String DEFAULT_MONGO_DB = "";
+    public static String DEFAULT_HIVE_DRIVER = "";
+    public static String HIVE_URL = "";
+    public static String HIVE_TEST_QUERY = "";
+
+    public DBProperties()
+    {
+    }
+
+}
