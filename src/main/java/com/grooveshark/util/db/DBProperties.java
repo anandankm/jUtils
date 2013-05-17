52fdc2b965624efd6487ccd0d56e1e399ff8ac83
Anandan <andy.compeer@gmail.com>
2013-05-17 15:39:06 -0400

    Load defaults from json file or defaults specified

--- src/main/java/com/grooveshark/util/db/DBProperties.java
+++ src/main/java/com/grooveshark/util/db/DBProperties.java
@@ -26,9 +26,11 @@ public class DBProperties
 
     protected String mysqlHost = "localhost";
     protected String mysqlDB = "mysql";
-    protected String mysqlOptions = "?allowMultiQueries=true";
+    protected String mysqlOptions = "";
 
     protected String mongoHost = "localhost";
+    protected String mongoDB = "test";
+    protected int mongoPort = 2718;
 
     public DBProperties() {
     }
@@ -39,22 +41,50 @@ public class DBProperties
         this.DEFAULT_MONGO_HOST = FileUtils.getJsonValue(je, "mongo_host");
         this.DEFAULT_MONGO_PORT = Integer.parseInt(FileUtils.getJsonValue(je, "mongo_port"));
         this.DEFAULT_MONGO_DB = FileUtils.getJsonValue(je, "mongo_db");
-        String mysqlHost = FileUtils.getJsonValue(je, "mysql_host");
-        String mysqlDB = FileUtils.getJsonValue(je, "mysql_db");
-        String mysqlOptions = FileUtils.getJsonValue(je, "mysql_options");
-        this.DEFAULT_MYSQL_URL = "jdbc:mysql://" + mysqlHost + ":3306/" + mysqlDB + mysqlOptions;
-        log.debug("Default mysql url: " + this.DEFAULT_MYSQL_URL);
-        this.DEFAULT_MYSQL_DRIVER = this.getMysqlDriver(je);
+        this.DEFAULT_MYSQL_URL = this.getJsonMysqlUrl(je);
+        this.DEFAULT_MYSQL_DRIVER = this.getJsonMysqlDriver(je);
     }
 
-    public String getMysqlDB(JsonElement je) throws Exception {
-        String mysql_db = FileUtils.getJsonValue(je, "mysql_db");
-        return mysql_db.equals("") ? this.mysqlDB : mysql_db;
+    public String getJsonMongoHost(JsonElement je) throws Exception {
+        String mongo_host = FileUtils.getJsonValue(je, "mongo_host");
+        return mongo_host.equals("") ? this.mongoHost : mongo_host;
     }
 
-    public String getMysqlDriver(JsonElement je) throws Exception {
+    public String getJsonMongoPort(JsonElement je) throws Exception {
+        String mongo_port = FileUtils.getJsonValue(je, "mongo_port");
+        return mongo_port.equals("") ? this.mongoPort : Integer.parseInt(mongo_port);
+    }
+
+    public String getJsonMongoDB(JsonElement je) throws Exception {
+        String mongo_db = FileUtils.getJsonValue(je, "mongo_db");
+        return mongo_db.equals("") ? this.mongoDB : Integer.parseInt(mongo_db);
+    }
+
+    public String getJsonMysqlDriver(JsonElement je) throws Exception {
         String mysql_driver = FileUtils.getJsonValue(je, "mysql_driver");
         return mysql_driver.equals("") ? this.DEFAULT_MYSQL_DRIVER : mysql_driver;
+    }
+
+    public String getJsonMysqlUrl(JsonElement je) throws Exception {
+        this.mysqlHost = this.getJsonMysqlHost(je);
+        this.mysqlDB = this.getJsonMysqlDB(je);
+        this.mysqlOptions = this.getJsonMysqlOptions(je);
+        return "jdbc:mysql://" + this.mysqlHost + ":3306/" + this.mysqlDB + this.mysqlOptions;
+    }
+
+    public String getJsonMysqlOptions(JsonElement je) throws Exception {
+        String mysql_options = FileUtils.getJsonValue(je, "mysql_options");
+        return mysql_options.equals("") ? this.mysqlOptions : mysql_options;
+    }
+
+    public String getJsonMysqlHost(JsonElement je) throws Exception {
+        String mysql_host = FileUtils.getJsonValue(je, "mysql_host");
+        return mysql_host.equals("") ? this.mysqlHost : mysql_host;
+    }
+
+    public String getJsonMysqlDB(JsonElement je) throws Exception {
+        String mysql_db = FileUtils.getJsonValue(je, "mysql_db");
+        return mysql_db.equals("") ? this.mysqlDB : mysql_db;
     }
 
     public void setDefaultsDsnFilename(String filename) {
@@ -108,5 +138,29 @@ public class DBProperties
      */
     public String getMongoHost() {
         return this.mongoHost;
+    }
+    /**
+     * Setter method for mongoPort
+     */
+    public void setMongoPort(int mongoPort) {
+        this.mongoPort = mongoPort;
+    }
+    /**
+     * Getter method for mongoPort
+     */
+    public int getMongoPort() {
+        return this.mongoPort;
+    }
+    /**
+     * Setter method for mongoDB
+     */
+    public void setMongoDB(String mongoDB) {
+        this.mongoDB = mongoDB;
+    }
+    /**
+     * Getter method for mongoDB
+     */
+    public String getMongoDB() {
+        return this.mongoDB;
     }
 }
