0ff9cd408a16ee83b39f094ce0415cbc5983e25a
Anandan <andy.compeer@gmail.com>
2013-05-24 21:41:19 -0400

    JUnit test for loading custom dsn

--- src/test/java/com/grooveshark/util/db/DBPropertiesTest.java
+++ src/test/java/com/grooveshark/util/db/DBPropertiesTest.java
@@ -10,16 +10,34 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.fail;
 
+import org.apache.log4j.Logger;
 
 public class DBPropertiesTest 
 {
 
+    public static final Logger log = Logger.getLogger(DBPropertiesTest.class);
     @Test
     public void loadDefaultsTest()
     {
         try {
             DBProperties dbp = new DBProperties();
             dbp.loadDefaults();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Failed to load defaults. Excpetion:\n"  + e.getMessage());
+        }
+    }
+
+    @Test
+    public void loadMysqlDsnTest()
+    {
+        try {
+            DBProperties dbp = new DBProperties();
+            String[] jsonHeader = {"dsn1"};
+            dbp.setMysqlDsn(jsonHeader);
+            log.debug("Mysql url: " + dbp.getMysqlURL());
+            log.debug("Mysql user: " + dbp.getMysqlUser());
+            log.debug("Mysql pass: " + dbp.getMysqlPass());
         } catch (Exception e) {
             e.printStackTrace();
             fail("Failed to load defaults. Excpetion:\n"  + e.getMessage());
