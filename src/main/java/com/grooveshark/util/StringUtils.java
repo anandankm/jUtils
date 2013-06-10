77dd3ae26e63627bf1c7cf9067209f0f8170b238
Anandan <andy.compeer@gmail.com>
2013-06-10 12:52:59 -0400

    Split strings and list them

--- src/main/java/com/grooveshark/util/StringUtils.java
+++ src/main/java/com/grooveshark/util/StringUtils.java
@@ -1,9 +1,23 @@
 package com.grooveshark.util;
 
+import java.util.Properties;
+import java.util.Enumeration;
+import java.io.InputStream;
+import java.io.IOException;
+
 
 public class StringUtils
 {
     public static void logToStdOut(String threadName, String msg) {
         System.out.println(DateUtils.getNow() + ": [" + threadName + "] " + msg);
+    }
+
+    public static LinkedList<String> splitTrim(String str, String sep) {
+        LinkedList<String> splitted = new LinkedList<String>();
+        String[] str = str.split(sep);
+        for(String st : str) {
+            splitted.add(st.trim());
+        }
+        return splitted;
     }
 }
