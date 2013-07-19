0ea8f3335412a041503d0e5e77fdb85742dbf470
Anandan <andy.compeer@gmail.com>
2013-07-19 19:52:14 -0400

    Add a parser for maxmind's geo city data
    o Store maxmind's country code, region code, city.
    o Parse and store everything as a hash map to search
      against given country, region, city strings.

---
+++ src/main/java/com/grooveshark/util/maxmind/Parser.java
@@ -0,0 +1,134 @@
+package com.grooveshark.util.maxmind;
+
+import com.grooveshark.util.maxmind.entity.LatLong;
+import com.grooveshark.util.FileUtils;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.text.ParseException;
+import java.io.IOException;
+
+public class Parser
+{
+    private List<String> cityData;
+    private List<String> regionData;
+
+    private Map<String, Object> country = new HashMap<String, Object>();
+    private Map<String, String> regionDataMap = new HashMap<String, String>();
+
+    public Parser(String cityFilename, String regionFilename)
+        throws IOException
+    {
+        this.cityData = FileUtils.readFile(cityFilename);
+        this.regionData = FileUtils.readFile(regionFilename);
+    }
+
+    public void parseData()
+        throws ParseException
+    {
+        this.parseRegionData();
+        this.parseCityData();
+    }
+
+    public void parseCityData()
+        throws ParseException
+    {
+        int linenumber = 0;
+        for (int i = 2; i < cityData.size(); i++) {
+            linenumber = i + 1;
+            String cityInfo = cityData.get(i);
+            String[] infoSplit = cityInfo.split(",");
+
+            if (infoSplit.length <= 6) {
+                throw new ParseException("City file has less than or equal to 6 comma separated values instead of 9: linenumber - " + linenumber + "; data - " + cityInfo, linenumber);
+            }
+
+            String countryCode = infoSplit[1].replaceAll("\"", "").trim();
+            if (countryCode.length() != 2) {
+                throw new ParseException("City file has country code length != 2: linenumber - " + linenumber + "; data - " + cityInfo, 1);
+            }
+
+            Map<String, Object> countryMap = this.getOrCreateMap(countryCode, this.country);
+            String regionCode = infoSplit[2].replaceAll("\"", "").trim();
+            String city = infoSplit[3].replaceAll("\"", "").trim().toLowerCase();
+            LatLong latLong = new LatLong();
+            latLong.latitude = Float.parseFloat(infoSplit[5].trim());
+            latLong.longitude = Float.parseFloat(infoSplit[6].trim());
+            /**
+             * If Region and City are empty, then take lat and long
+             * for the capital
+             */
+            if (regionCode.length() == 0 && city.length() == 0) {
+                countryMap.put("LatLong", latLong);
+                continue;
+            }
+            if (regionCode.length() != 2 && regionCode.length() != 0) {
+                throw new ParseException("City file has region code length != 2: linenumber - " + linenumber + "; data - " + cityInfo, 2);
+            }
+            String regionName = this.getRegionName(countryCode, regionCode);
+            Map<String, Object> regionsMap = this.getOrCreateMap("Regions", countryMap);
+            Map<String, Object> regionMap = this.getOrCreateMap(regionName, regionsMap);
+            /**
+             * If City name is empty, then take lat and long
+             * for the region
+             */
+            if (city.length() == 0) {
+                regionMap.put("LatLong", latLong);
+                continue;
+            }
+            Map<String, Object> citiesMap = this.getOrCreateMap("Cities", regionMap);
+            Map<String, Object> cityMap = this.getOrCreateMap(city, citiesMap);
+            String postalString = infoSplit[4].replaceAll("\"", "").trim();
+            /**
+             * If Postal/Zip string is empty, then take lat and long
+             * for the city
+             */
+            if (postalString.length() == 0) {
+                cityMap.put("LatLong", latLong);
+                continue;
+            }
+            Map<String, Object> postalsMap = this.getOrCreateMap("Postals", cityMap);
+            Map<String, Object> postalMap = this.getOrCreateMap(postalString, postalsMap);
+            postalMap.put("LatLong", latLong);
+        }
+    }
+
+    public Map<String, Object> getOrCreateMap(String key, Map<String, Object> source)
+    {
+            Map<String, Object> result = null;
+            if (source.containsKey(key)) {
+                result = (Map<String, Object>) source.get(key);
+            } else {
+                result= new HashMap<String, Object>();
+                source.put(key, result);
+            }
+            return result;
+    }
+
+    public String getRegionName(String countryCode, String regionCode)
+    {
+        return this.regionDataMap.get(countryCode + ":" + regionCode).toLowerCase();
+    }
+
+    public void parseRegionData()
+        throws ParseException
+    {
+        int linenumber = 0;
+        for (String regionInfo : this.regionData) {
+            linenumber++;
+            String[] infoSplit = regionInfo.split(",");
+            if (infoSplit.length <= 2) {
+                throw new ParseException("Region file has less than or equal to 2 comma separated values instead of 3: linenumber - " + linenumber + "; data - " + regionInfo, linenumber);
+            }
+            String cc = infoSplit[0].trim();
+            String rc = infoSplit[1].trim();
+            String regionName = infoSplit[2].replaceAll("\"", "").trim();
+            if (cc.length() <= 0 || rc.length() <= 0 || regionName.length() <= 0) {
+                throw new ParseException("Region file has invalid data: linenumber - " + linenumber + "; data - " + regionInfo, linenumber);
+            }
+            this.regionDataMap.put(cc + ":" + rc, regionName);
+        }
+    }
+}
---
+++ src/main/java/com/grooveshark/util/maxmind/entity/LatLong.java
@@ -0,0 +1,8 @@
+package com.grooveshark.util.maxmind.entity;
+
+
+public class LatLong
+{
+    public float latitude;
+    public float longitude;
+}
