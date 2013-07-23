9e08c3286eaca69e4b01c9e307c37fc00fca390c
Anandan <andy.compeer@gmail.com>
2013-07-23 00:19:45 -0400

    Command exec tester

---
+++ src/test/java/com/grooveshark/util/command/CommandTest.java
@@ -0,0 +1,45 @@
+package com.grooveshark.util.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+public class CommandTest 
+{
+
+    public static final Logger log = Logger.getLogger(CommandTest.class);
+
+    public static final String cityFilename = "data/GeoLiteCity_latest/allCountries.txt";
+
+    private CommandExecutor executor = null;
+
+    @Before 
+    public void setup()
+    {
+        this.executor = new CommandExecutor();
+    }
+
+    @Test
+    public void testParser()
+    {
+        long start = System.currentTimeMillis();
+        String[] cmdArray = new String[3];
+        cmdArray[0] = "grep" ;
+        cmdArray[1] = "chennai";
+        cmdArray[2] = cityFilename;
+        List<String> output = null;
+        try {
+            output = this.executor.execute(cmdArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Failed to execute command. Excpetion:\n"  + e.getMessage());
+        }
+        float elapsed = (System.currentTimeMillis() - start)/(float) 1000;
+        System.out.println("Done ["+elapsed+" secs].");
+        System.out.println("Output: " + output);
+    }
+
+}
