8d2cc69e78eda037011af4cafdd14724949d8979
Anandan <andy.compeer@gmail.com>
2013-07-26 14:52:26 -0400

    Calculate distance between 2 lat/long earth coordinates

---
+++ src/main/java/com/grooveshark/util/MathUtils.java
@@ -0,0 +1,58 @@
+package com.grooveshark.util;
+
+
+public class MathUtils 
+{
+    /**
+     * Earth's Radius in km
+     */
+    public static final int R = 6371;
+
+    /**
+     * Spherical Law of Cosines:
+     * Find distance in km between 2 latitude-longitude co-ordinates
+     * latitudes and longitudes as input are in decimal degrees wgs84.
+     *
+     * d = acos ( sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(long2-long1) ) * R
+     *
+     * Reference: http://www.movable-type.co.uk/scripts/latlong.html
+     */
+    public static double sphericalDistance(float latitude1, float longitude1, float latitude2, float longitude2)
+    {
+        double lat1 = Math.toRadians(latitude1);
+        double lat2 = Math.toRadians(latitude2);
+        double long1 = Math.toRadians(longitude1);
+        double long2 = Math.toRadians(longitude2);
+        return Math.acos(
+                (Math.sin(lat1) * Math.sin(lat2)) +
+                (Math.cos(lat1) * Math.cos(lat2) * Math.cos(long2-long1))
+                ) * MathUtils.R;
+    }
+
+    /**
+     * Haversine formula:
+     *
+     * Find distance in km between 2 latitude-longitude co-ordinates
+     * latitudes and longitudes as input are in decimal degrees wgs84.
+     *
+     * dlong = (long2 - long1)
+     * dlat = (lat2 - lat1)
+     * a = ( sin(dlat/2) )^2 + cos(lat1)*cos(lat2)*( sin(dlong/2) )^2
+     * c = 2 * atan2(sqrt(a), sqrt(1-a))
+     * d = c * R
+     *
+     * Reference: http://www.movable-type.co.uk/scripts/latlong.html
+     */
+    public static double haversineDistance(float latitude1, float longitude1, float latitude2, float longitude2)
+    {
+        double lat1 = Math.toRadians(latitude1);
+        double lat2 = Math.toRadians(latitude2);
+        double dlat = Math.toRadians(latitude2 - latitude1);
+        double dlong = Math.toRadians(longitude2 - longitude1);
+        double a = ( Math.sin(dlat/2) * Math.sin(dlat/2) ) +
+                   ( Math.sin(dlong/2) * Math.sin(dlong/2) * Math.cos(lat1) * Math.cos(lat2) );
+        double c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1 - a) );
+
+        return c * MathUtils.R;
+    }
+}
---
+++ src/test/java/com/grooveshark/util/MathUtilsTest.java
@@ -0,0 +1,41 @@
+package com.grooveshark.util;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.Logger;
+
+public class MathUtilsTest 
+{
+
+    public static final Logger log = Logger.getLogger(MathUtilsTest.class);
+
+    @Test
+    public void testDistance()
+    {
+        try {
+            // Gainesville
+            Float gainLat = 29.65163f;
+            Float gainLong = -82.32483f;
+            // Jacksonville
+            Float jackLat = 30.33218f;
+            Float jackLong = -81.65565f;
+            // Miami 
+            Float miamiLat = 25.77427f;
+            Float miamiLong = -80.19366f;
+            System.out.println("Gainesville - Jacksonville distance in km");
+            System.out.println("");
+            System.out.println("Spherical: " + MathUtils.sphericalDistance(gainLat, gainLong, jackLat, jackLong));
+            System.out.println("Haversine: " + MathUtils.haversineDistance(gainLat, gainLong, jackLat, jackLong));
+            System.out.println("");
+            System.out.println("Gainesville - Miami distance in km");
+            System.out.println("");
+            System.out.println("Spherical: " + MathUtils.sphericalDistance(gainLat, gainLong, miamiLat, miamiLong));
+            System.out.println("Haversine: " + MathUtils.haversineDistance(gainLat, gainLong, miamiLat, miamiLong));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Failed on distance test. Excpetion:\n"  + e.getMessage());
+        }
+    }
+
+}
